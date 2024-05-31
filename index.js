fef9997024e996f77a683a190021fc02d3d3e198
westhecool <52859033+westhecool@users.noreply.github.com>
2024-05-31 12:26:51 -0400

    fix code

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ if (!process.argv[2]) {
     process.exit(1);
 }
 const q = process.argv[2].split(',')
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+puppeteer.use(require('puppeteer-extra-plugin-user-preferences')({
+    userPrefs: {
+        download: {
+            prompt_for_download: false,
+            default_directory: process.cwd(),
+        }
+    }
+}));
+puppeteer.use(require('puppeteer-extra-plugin-adblocker')({ blockTrackers: true }))
 const fs = require('fs');
 const path = require('path');
 (async () => {
@@ -11,26 +20,41 @@ const path = require('path');
     const browser = await puppeteer.launch({ headless: false });
     for (const e of q) {
         const page = await browser.newPage();
-        await page.goto('https://apkcombo.com/downloader/#package=' + e);
-        const url = await page.evaluate(async () => {
-            var r = null;
-            while (r == null) {
-                for (const e of Array.from(document.getElementsByTagName('a'))) {
-                    if (e.href.startsWith('https://apkcombo.com/r2') || e.href.split('?')[0].endsWith('.apk')) {
-                        r = e.href;
-                        break;
-                    }
-                }
-                await new Promise(resolve => setTimeout(resolve, 100));
-            }
-            return r;
-        });
-        await page.close();
-        console.log(url);
-        const r = await fetch(url);
-        const buffer = Buffer.from(await r.arrayBuffer());
         await fs.promises.mkdir('apks/' + e, { recursive: true });
-        await fs.promises.writeFile('apks/' + e + '/' + path.basename(decodeURI(url.split('https://')[1].split('?')[0])), buffer);
+        await page.goto('https://apkcombo.com/downloader/#package=' + e);
+        var got = false;
+        while (!got) { // the site is buggy sometimes
+            await page.evaluate(async () => {
+                var r = false;
+                while (!r) {
+                    for (const e of Array.from(document.getElementsByTagName('a'))) {
+                        if (e.href.startsWith('https://apkcombo.com/r2') || e.href.split('?')[0].endsWith('.apk')) {
+                            window.location = e.href;
+                            r = true;
+                            break;
+                        }
+                    }
+                    await new Promise(resolve => setTimeout(resolve, 100));
+                }
+            });
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            for (const file of await fs.promises.readdir(process.cwd())) {
+                if (file.endsWith('.crdownload')) {
+                    got = true;
+                }
+            }
+        }
+        var d = false;
+        while (!d) {
+            for (const file of await fs.promises.readdir(process.cwd())) {
+                if (file.endsWith('.apk') || file.endsWith('.xapk')) {
+                    await fs.promises.rename(file, 'apks/' + e + '/' + file);
+                    d = true;
+                }
+            }
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        await page.close();
     }
     await browser.close();
 })();
